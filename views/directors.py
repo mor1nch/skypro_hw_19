23c6b70b1788526f19f52ec4608fbabd04c51053
mor1nch <step.denis.17@gmail.com>
2023-02-27 19:06:44 +0300

    updated views

--- views/directors.py
+++ views/directors.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restx import Resource, Namespace
 
 from dao.model.director import DirectorSchema
@@ -13,10 +14,26 @@ class DirectorsView(Resource):
         res = DirectorSchema(many=True).dump(rs)
         return res, 200
 
+    def post(self):
+        req_json = request.json
+        director = director_service.create(req_json)
+        return "", 201, {"location": f"/directors/{director.id}"}
 
-@director_ns.route('/<int:rid>')
+
+@director_ns.route('/<int:did>')
 class DirectorView(Resource):
-    def get(self, rid):
-        r = director_service.get_one(rid)
+    def get(self,did):
+        r = director_service.get_one(did)
         sm_d = DirectorSchema().dump(r)
         return sm_d, 200
+
+    def put(self, did):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = did
+        director_service.update(req_json)
+        return "Пользователь успешно обновлён", 204
+
+    def delete(self, did):
+        director_service.delete(did)
+        return "Пользователь успешно удалён", 204
